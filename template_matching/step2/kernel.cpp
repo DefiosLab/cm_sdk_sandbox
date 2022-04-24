df3a8da0bf5db253643b698d601a08fb5596ee12
Taiki Kondo <kontaiml@gmail.com>
2022-04-24 17:31:08 +0900

    refactor

--- template_matching/step2/kernel.cpp
+++ template_matching/step2/kernel.cpp
@@ -28,24 +28,23 @@ _GENX_MAIN_ void zncc(
     const uint32_t temp_size = temp_h*temp_w;
     int idx = cm_group_id(0)*cm_local_size(0) + cm_local_id(0);
     int idy = cm_group_id(1)*cm_local_size(1) + cm_local_id(1);
-    matrix<float, 8, 8> in;
-    matrix<float, 1, 1> temp;
-    matrix<float, 8, 8> out;
-    matrix<float, 8, 8> sum_src(0.0f);
-    matrix<float, 8, 8> sum_mul(0.0f);
-    matrix<float, 8, 8> sum_src_pw(0.0f);
-    matrix<float, 8, 8> m;
-    matrix<float, 8, 8> d;
-
+    matrix<float, block_y, block_x> in;
+    matrix<float, 1,1> temp;
+    matrix<float, block_y, block_x> out;
+    matrix<float, block_y, block_x> sum_src(0.0f);
+    matrix<float, block_y, block_x> sum_mul(0.0f);
+    matrix<float, block_y, block_x> sum_src_pw(0.0f);
+    matrix<float, block_y, block_x> m;
+    matrix<float, block_y, block_x> d;
    
    
     for(uint32_t i=0;i<temp_h;i++){
         for(uint32_t j=0;j<temp_w;j++){
-            read(sbuf, (idx*block_x+j)*sizeof(float), (idy*block_y)+i, in);
-            read(tbuf, j*sizeof(float), i, temp);
+            read(sbuf, (idx * block_x + j)*sizeof(float), (idy * block_y) + i, in);
+            read(tbuf, j *sizeof(float), i, temp);
             sum_src +=in;
-            sum_mul +=in*temp(0,0);
-            sum_src_pw += in*in;            
+            sum_mul +=in * temp(0,0);
+            sum_src_pw += in * in;            
         }
     }
     m = temp_size * sum_mul - sum_src * sum_temp;
@@ -53,7 +52,7 @@ _GENX_MAIN_ void zncc(
                        (temp_size*sum_temp_pw - sum_temp*sum_temp)));
 
     out = m/d;
-    write(obuf, idx*block_x*sizeof(float), idy*block_y, out);
+    write(obuf, idx * block_x * sizeof(float), idy * block_y, out);
 
 }
 
